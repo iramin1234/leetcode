7aa6cac55974c66666def4a2171930bd9cea311c
Ramin <iramin1234@gmail.com>
2022-08-10 00:43:43 -0400

    challenge 1 using py

---
+++ arrays/1_two_sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = {}
+        for i, val in enumerate(nums):
+            complement = target - val
+            if complement in nums_dict:
+                return [nums_dict[complement, i]]
+            nums_dict[val] = i
